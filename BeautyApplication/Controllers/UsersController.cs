d5dca77caec3f36a31cd698cff98d2d3102323c7
user16 <user16@user16.ru>
2023-03-02 14:07:27 +0500

    Создание метода авторизации

--- BeautyApplication/Controllers/UsersController.cs
+++ BeautyApplication/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using BeautyApplication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,16 @@ namespace BeautyApplication.Controllers
 {
     public  class UsersController
     {
+        public Users GetUser(string login, string password)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string url = Manager.RootUrl + "Users/" + login + "/" + password;
+                HttpResponseMessage response = client.GetAsync($"{url}").Result;
+                var content = response.Content.ReadAsStringAsync();
+                var answer = JsonConvert.DeserializeObject<Users>(content.Result);
+                return answer;
+            }
+        }
     }
 }
